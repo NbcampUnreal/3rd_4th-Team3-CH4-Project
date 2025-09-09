fd670345a554116f79fe48d418a1797dead54c72
DiooN <dioon0000@naver.com>
2025-09-09 16:20:54 +0900

    [추가] InputAction 및 IMC, BP 추가
    - 기본 캐릭터 IA, IMC 추가
    - 플레이어 캐릭터 및 데이터 에셋 DA 파일 추가

--- Source/Team03/Input/TTBaseCharacterInput.h
+++ Source/Team03/Input/TTBaseCharacterInput.h
@@ -6,9 +6,20 @@
 #include "Engine/DataAsset.h"
 #include "TTBaseCharacterInput.generated.h"
 
+class UInputAction;
+
 UCLASS()
 class TEAM03_API UTTBaseCharacterInput : public UDataAsset
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UInputAction> Move;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UInputAction> Sprint;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UInputAction> Jump;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UInputAction> Look;
 };
