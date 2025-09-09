b97bffc7b921ae01242e346f82e3532224a158ae
Hyeongcheol <gudcjf6543@gmail.com>
2025-09-09 11:37:57 +0900

    OutGame UI 추가

---
+++ Source/Team03/OutGameUI/TTMainMenuWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TTMainMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM03_API UTTMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
